c300e5a1383ec71b568cc6ec9772f09549a27334
417-72KI <417.72ki@gmail.com>
2022-05-19 18:51:30 +0900

    bump swift-argument-parser to 1.1.2

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "xcode-arch", targets: ["XcodeArch"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
