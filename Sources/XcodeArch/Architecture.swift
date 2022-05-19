f8e84475b5a2d57512f078fe4a86567dbe73365f
417-72KI <417.72ki@gmail.com>
2022-05-19 14:48:16 +0900

    divide files

---
+++ Sources/XcodeArch/Architecture.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+enum Architecture: String, CaseIterable {
+    case x86_64
+    case arm64
+}
--- Sources/XcodeArch/XcodeArch.swift
+++ Sources/XcodeArch/XcodeArch.swift
@@ -62,33 +62,3 @@ private extension XcodeArch {
         return url.path
     }
 }
-
-enum Architecture: String, CaseIterable {
-    case x86_64
-    case arm64
-}
-
-enum XcodeArchError: Error {
-    case runningInX86_64
-    case invalidArchitecture(String)
-    case unknownXcodePath
-    case launchServicesPlistNotFound(URL)
-    case invalidPlist
-}
-
-extension XcodeArchError: CustomStringConvertible {
-    var description: String {
-        switch self {
-        case .runningInX86_64:
-            return "Running in `x86_64` arch. This tool supports only `arm64`"
-        case let .invalidArchitecture(actual):
-            return "Invalid architecture: \(actual)"
-        case .unknownXcodePath:
-            return "Xcode is not installed"
-        case let .launchServicesPlistNotFound(expected):
-            return "\(expected) not found."
-        case .invalidPlist:
-            return "Invalid plist"
-        }
-    }
-}
